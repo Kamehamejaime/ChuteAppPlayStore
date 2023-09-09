06e25dadb4cc86f2298d2495378217b20d890d57
Kamehamejaime <jaime.catalanc@gmail.com>
2023-09-09 15:40:18 -0300

    se aqgrega vista 'Equipos' .OK

--- app/src/main/java/com/example/chuteapp/Equipos.java
+++ app/src/main/java/com/example/chuteapp/Equipos.java
@@ -1,25 +1,18 @@
 package com.example.chuteapp;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Ingreso extends AppCompatActivity {
+public class Equipos extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
-
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicio);
+        setContentView(R.layout.activity_equipos);
 
     }
-    public void onClickEquipos(View view){
-        Intent intent = new Intent(this,Equipos.class);
-        startActivity(intent);
-    }
+
 
 }
