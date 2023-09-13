12c14294451dfa89dd11b50ea1b178de1340fa4a
Kamehamejaime <jaime.catalanc@gmail.com>
2023-09-12 22:37:44 -0300

    xd

--- app/src/main/java/com/example/chuteapp/Perfil.java
+++ app/src/main/java/com/example/chuteapp/Perfil.java
@@ -1,7 +1,9 @@
 package com.example.chuteapp;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,5 +16,15 @@ public class Perfil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
+    }
+
+    public void onClickCrear(View view){
+        Intent intent = new Intent(this,Crear.class);
+        startActivity(intent);
+    }
+
+    public void onClickEquipos(View view){
+        Intent intent = new Intent(this,Equipos.class);
+        startActivity(intent);
     }
 }
