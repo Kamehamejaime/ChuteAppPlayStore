07b848b98702795aefb6d95208c65946888e9027
PateNote <andresbcarvajal4982@gmail.com>
2023-10-21 12:59:35 -0300

    feat Marcador: Se añade modelo de Marcador para un trabajo más eficiente.

---
+++ app/src/main/java/com/example/chuteapp/models/Marcador.java
@@ -0,0 +1,32 @@
+package com.example.chuteapp.models;
+
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+public class Marcador {
+    MapView mapView;
+    Marker marker;
+    GeoPoint point;
+    String title;
+    String snippet;
+
+    public Marcador(MapView mapView, double latitude, double longitude, String title, String snippet) {
+        point = new GeoPoint(latitude, longitude);
+        this.title = title;
+        this.snippet = snippet;
+        marker = new Marker(mapView);
+        marker.setPosition(point);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        marker.setTitle(this.title);
+        marker.setSnippet(this.snippet);
+    }
+
+    public void showMarker(){
+        mapView.getOverlays().add(marker);
+    }
+
+    public void hideMarker(){
+        mapView.getOverlays().clear();
+    }
+}
